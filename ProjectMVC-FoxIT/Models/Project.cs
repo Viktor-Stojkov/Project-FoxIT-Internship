d953d3cce6584f4e3fa79bdf5d89c95302fefdae
Viktor-Stojkov <viktor.stojkov93@gmail.com>
2022-01-18 15:03:27 +0100

    The project is ready

---
+++ ProjectMVC-FoxIT/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProjectMVC_FoxIT.Models
+{
+    public partial class Project
+    {
+        public Project()
+        {
+            WorkOrders = new HashSet<WorkOrder>();
+        }
+
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public int CustomerId { get; set; }
+        public bool? IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual ICollection<WorkOrder> WorkOrders { get; set; }
+    }
+}
